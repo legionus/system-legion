ea9c257be32b2875f75b5b57e3334c96cd65c596
Alexey Gladkov <legion@altlinux.org>
2023-08-06 22:26:43 +0200

    Remove upstreamed code
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- system/run-image-patches.mk
+++
@@ -1,6 +0,0 @@
-CHROOT_IMAGE_PATCHES ?= $(CURDIR)/mki-image-patches
-MKI_IMAGE_PATCHDIR ?= $(CURDIR)/image-patches.d
-
-run-image-patches: prepare-image-workdir $(SUBDIRS)
-	@echo "mkimage: Processing '$@' ..."
-	@$(CHROOT_IMAGE_PATCHES)
