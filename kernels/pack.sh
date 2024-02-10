1eb70b0a977ee466f494372ff860b02089147f19
Alexey Gladkov <legion@altlinux.org>
2024-02-10 15:56:23 +0000

    kernel: Handle systemd-boot kernel layout
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- kernels/pack.sh
+++ kernels/pack.sh
@@ -16,6 +16,50 @@ print_path() {
 	echo "** $prefix: $name [$size] ($checksum)" >&2
 }
 
+pack_kernel() {
+	local outdir i vmlinuz initrd flavour moddir
+
+	i="$1"; shift
+	vmlinuz="$1"; shift
+	initrd="$1"; shift
+
+	flavour=""
+
+	if [ -z "${i##*-*}" ]; then
+		flavour="${i%-*}"
+		flavour="${flavour#*-}"
+	fi
+
+
+	echo packing kernel "$i" >&2
+
+	outdir="/.host/out/kernel-$i"
+	mkdir -p -- "$outdir"
+
+	cp -f -- "$vmlinuz" "$outdir/vmlinuz-$i"
+	cp -f -- "$initrd"  "$outdir/initrd-$i.img"
+
+	if [ -n "$flavour" ]; then
+		ln -snf vmlinuz-"$i"    "$outdir"/vmlinuz-"$flavour"
+		ln -snf initrd-"$i".img "$outdir"/initrd-"$flavour".img
+	fi
+	ln -snf vmlinuz-"$i"    "$outdir"/vmlinuz
+	ln -snf initrd-"$i".img "$outdir"/initrd.img
+
+	moddir=lib/modules
+	[ ! -L lib ] || moddir="$(readlink lib)/modules"
+
+	tar --numeric-owner "$@" \
+		--xattrs \
+		-cf - "$moddir/$i" |
+		sqfstar -b 1M -comp xz "$outdir/$modname"
+
+	print_path kernel "$outdir"/vmlinuz-"$i"
+	print_path initrd "$outdir"/initrd-"$i".img
+	print_path modules "$outdir/$modname"
+}
+
+
 cd /.image
 
 set --
@@ -25,44 +69,31 @@ if [ -s ./.SOURCE_DATE_EPOCH ]; then
 	set -- --clamp-mtime --mtime=@"$SOURCE_DATE_EPOCH"
 fi
 
+found=
+
+for cfg in boot/loader/entries/*.conf; do
+	[ -f "$cfg" ] || continue
+
+	version="$(sed -rn -e 's/^version[[:space:]]+//p' "$cfg")"
+	vmlinuz="$(sed -rn -e 's/^linux[[:space:]]+//p' "$cfg")"
+
+	pack_kernel "$version" "./$vmlinuz" boot/initrd-"$version".img "$@"
+	found=1
+done
+
 for i in boot/vmlinuz-*; do
-	if [ ! -e "$i" ]; then
-		echo >&2 "ERROR: unable to find /boot/vmlinuz-<KVER>"
-		exit 1
-	fi
+	[ -e "$i" ] || continue
 
 	[ ! -L "$i" ] ||
 		continue
 
 	i="${i#boot/vmlinuz-}"
-	flavour=""
 
-	if [ -z "${i##*-*}" ]; then
-		flavour="${i%-*}"
-		flavour="${flavour#*-}"
-	fi
-
-	echo packing kernel "$i" >&2
-
-	outdir="/.host/out/kernel-$i"
-	mkdir -p -- "$outdir"
-
-	cp -t "$outdir" boot/vmlinuz-"$i"
-	cp -t "$outdir" boot/initrd-"$i".img
-
-	if [ -n "$flavour" ]; then
-		ln -snf vmlinuz-"$i"    "$outdir"/vmlinuz-"$flavour"
-		ln -snf initrd-"$i".img "$outdir"/initrd-"$flavour".img
-	fi
-	ln -snf vmlinuz-"$i"    "$outdir"/vmlinuz
-	ln -snf initrd-"$i".img "$outdir"/initrd.img
-
-	tar --numeric-owner "$@" \
-		--xattrs \
-		-cf - lib/modules/"$i" |
-		sqfstar -b 1M -comp xz "$outdir/$modname"
-
-	print_path kernel "$outdir"/vmlinuz-"$i"
-	print_path initrd "$outdir"/initrd-"$i".img
-	print_path modules "$outdir/$modname"
+	pack_kernel "$i" boot/vmlinuz-"$i" boot/initrd-"$i".img "$@"
+	found=1
 done
+
+if [ -z "$found" ]; then
+	echo >&2 "ERROR: unable to find kernel and initramfs"
+	exit 1
+fi
